8c2c106710a5475dbb2861f90e026a72a6fe6e14
Ettore <ettorealoe02@gmail.com>
2024-07-08 10:53:18 +0200

    first commint

---
+++ src/main/java/com/example/progpi/ProgPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.progpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/progpi/ProgPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
